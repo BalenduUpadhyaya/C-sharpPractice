e15f0050390ad1c3069161f6d9ea9ba8bde9636e
balenduupadhyaya <balendu.upadhyaya@successive.tech>
2021-05-10 14:20:25 +0530

    10-05-2021

--- Practice Codes/Program.cs
+++ Practice Codes/Program.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace Practice_Codes
 {
+    // yeh ak class banyi hamne 2 attributes ke sath
+    
+    class Player
+    {
+        //public string name = Console.ReadLine();
+        //public int health = Convert.ToInt32(Console.ReadLine());
+        //public void age(int a)
+        //{
+        //   int g = a;
+        //    Console.WriteLine("age is " + g);
+        //}
+
+        //// get set method usage
+        //public int gethealth()
+        //{
+        //    return health;
+        //}
+        //public void sethealth(int h)
+        //{
+        //    health = h;
+        //}
+    }
+    
+    
     class Program
     {
+        //This is a function or Method
+
+
+        //static void greet()          //Void ka matlb ki kuch return ni kr rha hai Jis trike ki value return kreneg vo rikhenge static ke agge
+        //{
+        //    Console.WriteLine("HI");
+        //}
+
+
+        //static int average(int a , int b , int c )
+        //{
+        //    int sum = a + b + c;
+        //    return sum;
+        //}
         static void Main(string[] args)
         {
             //int balendu = 10; //integer Variable 
@@ -108,14 +146,97 @@ namespace Practice_Codes
             //string hi = "Hello i am from \" ghaziabad"; // agar bich mai koi bhi quotes ya kuch dalna hai toh ak backslash ka use ho jyega or new line ka lia \n
             //Console.WriteLine(hi);
 
+            //---------------------If Else Else if ---------------------------->> If else ladder
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a > 18)
+            //{
+            //    Console.WriteLine("Above 18");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No u cant drive");
+            //}
+
+
+            //----------Switch-------------//
+
+            //int age =Convert.ToInt32(Console.ReadLine());
+
+            //switch(age)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Hi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Hello");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enjoy");
+            //            break;
+
+            //}
+            //Console.ReadLine();
+
+            //--------------Loops in C Sharp--------------->>
+
+            //1- While Loop
+
+            //int i = 0;
+            //while(i<5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+            //2. Do while lopp isme kis hota hai ki ab toh chlta hi hai function 
+            //int a = 0;
+            //do
+            //{
+            //    Console.WriteLine(a + 1);
+            //    a++;
+            //} while (a > 5);
+            //Console.ReadLine();
+
+            //3. For Loop
+
+            //for(int a=0; a<5; a++)
+            //{
+            //    Console.WriteLine(a + 1);
+            //    break;
+            //}
+            //Console.ReadLine();
+
+            //Methods or Function
 
 
 
+            // gm("Abhishek");
+            //Console.WriteLine(average(1, 2, 3));
 
+            //OOps -Classes and Objects
+            //Yeh hamne player class ki ak object banyi abhishek name se
             
             
+            //Player abhishek = new Player();
+            //Console.WriteLine(abhishek.name);
+            //Console.WriteLine(abhishek.gethealth());
+            //abhishek.sethealth(15);
+            //Console.WriteLine(abhishek.gethealth());
+            //abhishek.age(100);
+            //Console.ReadLine();
 
-            
+
+
+
+
+
+
+
+
+
+
 
 
         }
