39e5f519282f0f9f0edef67a05077e1b6694d83a
balenduupadhyaya <balendu.upadhyaya@successive.tech>
2021-05-15 16:10:52 +0530

    Add Two number using array

---
+++ Practice Codes/Add Two No using Array .cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Hello
+{
+    class Array
+    {
+         static void Main (string[] Args )
+        {
+            int[] a = new int[5];
+            int[] b = new int[5];
+            int[] c = new int[5];
+
+            for(int i = 0; i < 5; i++)
+            {
+             a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            b[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                c[i] = a[i] + b[i];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(c[i]+" ");
+            }
+            
+        }
+    }
+}
